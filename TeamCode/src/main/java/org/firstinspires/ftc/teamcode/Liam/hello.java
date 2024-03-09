ecbcfe24b6f3e245f2e2275b1c5eaa89db15af7f
wazlo <liamwheld@gmail.com>
2024-03-08 19:55:25 -0500

    commit push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Liam/hello.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Liam/hello.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.Liam;
 
-public @interface HelloWorld  Liam
-{
+public class hello {
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/luca/helloworld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/luca/helloworld.java
@@ -3,9 +3,6 @@ package org.firstinspires.ftc.teamcode.luca;
 public class helloworld {
 }
 
-while happy = true
-    explode
-
 
 
 
