94abfbbf8230f9751f56640d210e2df0375956e9
Robocavs-Gold <robocavs8479@gmail.com>
2024-03-08 20:21:46 -0500

    fdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gavin/Sample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gavin/Sample.java
@@ -5,8 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp (name= "helloworldgavin")
 public class Sample extends LinearOpMode {
-
-
     @Override
     public void runOpMode() throws InterruptedException {
 
