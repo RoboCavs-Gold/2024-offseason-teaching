43dea291657d8de0f4378d87fa4a65569f0f5514
shriya2222 <shriya.ramsingh@gmail.com>
2024-03-15 18:10:47 -0400

    My initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shriya/helloworld.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Shriya;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp (name="helloworldshriya")
+public class helloworld extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
+
+
