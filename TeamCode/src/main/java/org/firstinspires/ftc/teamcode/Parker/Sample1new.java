3123d0e22dae061ea58259ce07736074d9fcacc0
pjmugzly <pjreeser2007@gmail.com>
2024-03-15 16:22:00 -0700

    Made some good code, better than YOU LUCA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parker/Sample1new.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parker/Sample1new.java
@@ -2,13 +2,46 @@ package org.firstinspires.ftc.teamcode.Parker;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorImplEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp (name= "HelloworldParker")
 public class Sample1new extends LinearOpMode {
 
+    public DcMotorEx fl, fr, bl, br;
     @Override
     public void runOpMode() throws InterruptedException {
 
+        fl = hardwareMap.get(DcMotorImplEx.class, "FLMotor");
+        fr = hardwareMap.get(DcMotorImplEx.class, "FRMotor");
+        bl = hardwareMap.get(DcMotorImplEx.class, "BLMotor");
+        br = hardwareMap.get(DcMotorImplEx.class, "BRMotor");
+
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        while (opModeIsActive() && !isStopRequested())   {
+            //code will run here while opmode is active }
+            double flPower = +gamepad1.left_stick_x - gamepad1.left_stick_y + gamepad1.right_stick_x;
+            double frPower = -gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x;
+            double blPower = -gamepad1.left_stick_x - gamepad1.left_stick_y + gamepad1.right_stick_x;
+            double brPower = +gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x;
+
+            fl.setPower(flPower);
+            fr.setPower(frPower);
+            bl.setPower(blPower);
+            br.setPower(brPower);
+
+            if (gamepad1.left_bumper)   {
+    fl.setPower(3.0);
+    fr.setPower(3.0);
+    bl.setPower(3.0);
+    br.setPower(3.0);
+
+            }
+            }
     }
 
 }
